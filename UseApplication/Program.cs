f285f3d0940934e79784a2596edf01e94cbb92be
David <david.eglem@gmail.com>
2013-01-27 13:49:25 +0100

    Serialization work almost
    You can serilize Document but not its derived types yet...
    -->Document.xml is created in Mediasharp/UseApplication/bin/Debug

--- UseApplication/Program.cs
+++ UseApplication/Program.cs
@@ -8,6 +8,10 @@ using MediaSharp.Controller;
 using MediaSharp.Model;
 using MediaSharp.View;
 
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
 namespace UseApplication
 {
     static class Program
@@ -25,13 +29,52 @@ namespace UseApplication
             m.AddDocument(new Audio("Regarde", "Comme je suis", new Author("trop", "fort"), true, 700));
             m.AddDocument(new Video("hein", "hein", new Author("hein", "hein?"), false, 600));
             
+            /****Serialization test - START*****/ //Works only with "Document", currently working on making it works with derived types...
+            /*UseApplication.Program.SerializeToXML(m);
+
+            Mediatheque mCpy = UseApplication.Program.DeserializeFromXML();*/
+            /****Serialization test - END*****/
 
             View_mdtq view = new View_mdtq();
             view.Visible = false;
 
+            //Ctrl_mediatheque controller = new Ctrl_mediatheque(mCpy, view);
             Ctrl_mediatheque controller = new Ctrl_mediatheque(m, view);
             controller.LoadView();
             view.ShowDialog();
+        }
+
+        static public void SerializeToXML(Mediatheque mediatheque)
+        {
+            FileStream fsOverwrite = new FileStream("Document.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(Mediatheque));
+            TextWriter textWriter = new StreamWriter(fsOverwrite);
+            try
+            {
+                serializer.Serialize(textWriter, mediatheque);
+            }
+            finally
+            {
+                textWriter.Close();
+                fsOverwrite.Close();
+            }
+        }
+
+        static Mediatheque DeserializeFromXML()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Mediatheque));
+            TextReader textReader = new StreamReader("Document.xml", true);
+            Mediatheque mediatheque;
+            try
+            {
+                mediatheque = (Mediatheque)deserializer.Deserialize(textReader);
+            }
+            finally
+            {
+                textReader.Close();
+            }
+
+            return mediatheque;
         }
     }
 }
