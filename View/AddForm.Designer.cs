eaaf130efd9ec4b9565ad3ad2472130110e9cae2
Pinto Jérémy <jeremy.pinto82@gmail.com>
2013-02-04 11:39:21 +0100

    Vrai ajout de l'ajout haha

---
+++ View/AddForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace View
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.cbBType = new System.Windows.Forms.ComboBox();
+            this.panelDetails = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.panelDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(65, 237);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(146, 236);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtTitle.Location = new System.Drawing.Point(12, 12);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(165, 20);
+            this.txtTitle.TabIndex = 2;
+            this.txtTitle.Text = "Title of the document";
+            this.txtTitle.MouseLeave += new System.EventHandler(this.txtTitle_MouseLeave);
+            this.txtTitle.MouseHover += new System.EventHandler(this.txtTitle_MouseHover);
+            // 
+            // cbBType
+            // 
+            this.cbBType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbBType.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cbBType.FormattingEnabled = true;
+            this.cbBType.Items.AddRange(new object[] {
+            "Article",
+            "Audio",
+            "Book",
+            "Multimedia",
+            "Video"});
+            this.cbBType.Location = new System.Drawing.Point(13, 67);
+            this.cbBType.Name = "cbBType";
+            this.cbBType.Size = new System.Drawing.Size(121, 21);
+            this.cbBType.TabIndex = 3;
+            this.cbBType.Text = "Type of Document";
+            this.cbBType.SelectedIndexChanged += new System.EventHandler(this.cbBType_SelectedIndexChanged);
+            // 
+            // panelDetails
+            // 
+            this.panelDetails.Controls.Add(this.groupBox1);
+            this.panelDetails.Location = new System.Drawing.Point(13, 94);
+            this.panelDetails.Name = "panelDetails";
+            this.panelDetails.Size = new System.Drawing.Size(208, 136);
+            this.panelDetails.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(3, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(196, 133);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAuthor.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtAuthor.Location = new System.Drawing.Point(13, 38);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(165, 20);
+            this.txtAuthor.TabIndex = 5;
+            this.txtAuthor.Text = "Author of the document";
+            this.txtAuthor.MouseLeave += new System.EventHandler(this.txtAuthor_MouseLeave);
+            this.txtAuthor.MouseHover += new System.EventHandler(this.txtAuthor_MouseHover);
+            // 
+            // timePicker
+            // 
+            this.timePicker.Location = new System.Drawing.Point(0, 0);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.Size = new System.Drawing.Size(200, 20);
+            this.timePicker.TabIndex = 0;
+            // 
+            // AddForm
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Dialog;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(236, 271);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.panelDetails);
+            this.Controls.Add(this.cbBType);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnCancel);
+            this.Name = "AddForm";
+            this.Text = "addForm";
+            this.panelDetails.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ComboBox cbBType;
+        private System.Windows.Forms.Panel panelDetails;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.TextBox txtReEditor;
+        private System.Windows.Forms.TextBox txtReTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown spinReviewNumber;
+    }
+}
---
+++ View/AddForm.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MediaSharp.Controller;
+
+namespace View
+{
+    public partial class AddForm : Form
+    {
+        Ctrl_mediatheque controller;
+
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Events and stuffs
+
+        private void txtTitle_MouseHover(object sender, EventArgs e)
+        {
+           if (txtTitle.Text == "Title of the document")
+            {
+                txtTitle.Text = "";
+                txtTitle.ForeColor = SystemColors.WindowText;
+                txtTitle.Font = SystemFonts.DefaultFont;
+            }
+        }
+
+        private void txtTitle_MouseLeave(object sender, EventArgs e)
+        {
+            if (txtTitle.Text == "")
+            {
+                txtTitle.Text = "Title of the document";
+                txtTitle.ForeColor = SystemColors.InactiveCaption;
+                txtTitle.Font = SystemFonts.CaptionFont;
+            }
+        }
+
+        private void cbBType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbBType.ForeColor = SystemColors.WindowText;
+            panelDetails_Load(cbBType.SelectedItem.ToString());
+        }
+
+        private void txtAuthor_MouseHover(object sender, EventArgs e)
+        {
+            if (txtAuthor.Text == "Author of the document")
+            {
+                txtAuthor.Text = "";
+                txtAuthor.ForeColor = SystemColors.WindowText;
+                txtAuthor.Font = SystemFonts.DefaultFont;
+            }
+        }
+
+        private void txtAuthor_MouseLeave(object sender, EventArgs e)
+        {
+            if (txtAuthor.Text == "")
+            {
+                txtAuthor.Text = "Author of the document";
+                txtAuthor.ForeColor = SystemColors.InactiveCaption;
+                txtAuthor.Font = SystemFonts.CaptionFont;
+            }
+        }
+
+        /* TabInfos[i] for i =
+         * 0 : Title of the document
+         * 1 : Author name and surname
+         * 2 : Copyright
+         * 3 : Type
+         * -- Audio & Video --
+         * 4 : duration
+         * -- Book --
+         * 4 : editor
+         * 5 : publication year
+         * -- Article --
+         * 4 : review title
+         * 5 : review editor
+         * 6 : review number
+         * -- Multimedia --
+         * Chaud du zboob
+         */
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            List<string> infos = new List<string>();
+            infos.Add(txtTitle.Text);
+            infos.Add(txtAuthor.Text);
+            infos.Add("false");
+            //Will make the tab contain the info of the document switch the type
+            switch (cbBType.SelectedItem.ToString())
+            {
+                case "Audio":
+                    infos.Add(timePicker.Text);
+                    break;
+                default:
+                    Console.WriteLine("PAS BON DE PRINT CA");
+                    break;
+            }
+            controller.AddNewDocument(infos.ToArray());
+            this.Close();
+        }
+
+
+        #endregion
+
+        public void SetController(Ctrl_mediatheque _controller)
+        {
+            controller = _controller;
+        }
+
+        private void panelDetails_Load (string type)
+        {
+            switch (type)
+            {
+                case "Audio":
+                    Console.WriteLine("Panel detail : " + type);
+                    // 
+                    // groupBox1
+                    // 
+                    this.groupBox1.Controls.Add(this.label1);
+                    this.groupBox1.Controls.Add(this.timePicker);
+                    this.groupBox1.Location = new System.Drawing.Point(-8, 0);
+                    this.groupBox1.Name = "groupBox1";
+                    this.groupBox1.Size = new System.Drawing.Size(327, 46);
+                    this.groupBox1.TabIndex = 0;
+                    this.groupBox1.TabStop = false;
+                    this.groupBox1.Text = type;
+                    // 
+                    // timePicker
+                    // 
+                    this.timePicker.CustomFormat = "H:M:S";
+                    this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+                    this.timePicker.Location = new System.Drawing.Point(74, 19);
+                    this.timePicker.Name = "timePicker";
+                    this.timePicker.ShowUpDown = true;
+                    this.timePicker.Size = new System.Drawing.Size(68, 20);
+                    this.timePicker.TabIndex = 0;
+                    this.timePicker.Value = new System.DateTime(2013, 1, 28, 0, 0, 0, 0);
+                    //Resize buttons
+                    this.btnCancel.Location = new System.Drawing.Point(195, 149);
+                    this.btnAdd.Location = new System.Drawing.Point(276, 148);
+                    //Resize the window
+                    this.ClientSize = new System.Drawing.Size(363, 176);
+
+                    break;
+                case "Article":
+                    // 
+                    // groupBox1
+                    // 
+                    this.groupBox1.Controls.Add(this.label1);
+                    this.groupBox1.Controls.Add(this.spinReviewNumber);
+                    this.groupBox1.Controls.Add(this.txtReEditor);
+                    this.groupBox1.Controls.Add(this.txtReTitle);
+                    this.groupBox1.Location = new System.Drawing.Point(3, 0);
+                    this.groupBox1.Name = "groupBox1";
+                    this.groupBox1.Size = new System.Drawing.Size(196, 133);
+                    this.groupBox1.TabIndex = 0;
+                    this.groupBox1.TabStop = false;
+                    // 
+                    // textBox2
+                    // 
+                    this.txtReEditor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    this.txtReEditor.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+                    this.txtReEditor.Location = new System.Drawing.Point(9, 46);
+                    this.txtReEditor.Size = new System.Drawing.Size(152, 20);
+                    this.txtReEditor.TabIndex = 3;
+                    this.txtReEditor.Text = "Editor\'s Name";
+                    // 
+                    // textBox1
+                    // 
+                    this.txtReTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    this.txtReTitle.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+                    this.txtReTitle.Location = new System.Drawing.Point(9, 20);
+                    this.txtReTitle.Size = new System.Drawing.Size(152, 20);
+                    this.txtReTitle.TabIndex = 2;
+                    this.txtReTitle.Text = "Review\'s Title";
+                    // 
+                    // spinReviewNumber
+                    // 
+                    this.spinReviewNumber.Location = new System.Drawing.Point(130, 72);
+                    this.spinReviewNumber.Maximum = new decimal(new int[] {
+                    500,
+                    0,
+                    0,
+                    0});
+                    this.spinReviewNumber.Minimum = new decimal(new int[] {
+                    1,
+                    0,
+                    0,
+                    0});
+                    this.spinReviewNumber.Name = "spinReviewNumber";
+                    this.spinReviewNumber.Size = new System.Drawing.Size(30, 20);
+                    this.spinReviewNumber.TabIndex = 6;
+                    this.spinReviewNumber.Value = new decimal(new int[] {
+                    1,
+                    0,
+                    0,
+                    0});
+                    // 
+                    // label1
+                    // 
+                    this.label1.AutoSize = true;
+                    this.label1.Location = new System.Drawing.Point(6, 74);
+                    this.label1.Size = new System.Drawing.Size(90, 13);
+                    this.label1.TabIndex = 7;
+                    this.label1.Text = "Review\'s Number";
+                    //Resize butts
+                    this.btnCancel.Location = new System.Drawing.Point(65, 237);
+                    this.btnAdd.Location = new System.Drawing.Point(146, 236);
+                    //Resize window
+                    this.ClientSize = new System.Drawing.Size(236, 271);
+                    break;
+                case "Book":
+                    break;
+                case "Multimedia":
+                    break;
+                case "Video":
+                    // 
+                    // groupBox1
+                    // 
+                    this.groupBox1.Controls.Add(this.label1);
+                    this.groupBox1.Controls.Add(this.timePicker);
+                    this.groupBox1.Location = new System.Drawing.Point(-8, 0);
+                    this.groupBox1.Name = "groupBox1";
+                    this.groupBox1.Size = new System.Drawing.Size(327, 46);
+                    this.groupBox1.TabIndex = 0;
+                    this.groupBox1.TabStop = false;
+                    this.groupBox1.Text = type;
+                    // 
+                    // timePicker
+                    // 
+                    this.timePicker.CustomFormat = "H:M:S";
+                    this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+                    this.timePicker.Location = new System.Drawing.Point(74, 19);
+                    this.timePicker.Name = "timePicker";
+                    this.timePicker.ShowUpDown = true;
+                    this.timePicker.Size = new System.Drawing.Size(68, 20);
+                    this.timePicker.TabIndex = 0;
+                    this.timePicker.Value = new System.DateTime(2013, 1, 28, 0, 0, 0, 0);
+                    break;
+            }
+            this.panelDetails.Controls.Add(this.groupBox1);
+            this.panelDetails.Size = new System.Drawing.Size(338, 49);
+            this.groupBox1.PerformLayout();
+        }
+    }
+}
