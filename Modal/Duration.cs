8135d1dc1fb3d5e9d62bada195ea46fe13e2f531
David <david.eglem@gmail.com>
2013-02-07 13:00:38 +0100

    Resolution du bug de serialization

--- Modal/Duration.cs
+++ Modal/Duration.cs
@@ -12,10 +12,12 @@ namespace MediaSharp.Model
         private int minutes;
         private int secondes;
 
+        #region GETTERS & SETTERS
         public int Hours { get { return hours; } set { if (value > 0) { hours = value; } else { hours = 0; } } }
         public int Minutes { get { return minutes; } set { if (value > 0) { minutes = value; } else { minutes = 0; } } }
         public int Secondes { get { return secondes; } set { if (value > 0) { secondes = value; } else { secondes = 1; } } }
-
+        #endregion
+        #region CONSTRUCTORS
         public Duration(int h, int m, int s)
         {
             Hours = h;
@@ -23,7 +25,13 @@ namespace MediaSharp.Model
             Secondes = s;
         }
 
-        
+        public Duration()
+        {
+            Hours = 0;
+            Minutes = 0;
+            Secondes = 0;
+        }
+        #endregion
 
     }
 }
